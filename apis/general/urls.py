05cb3b4c16e270690f52e3ac4f279ad91578e7c2
ojitos369 <ojitos369@gmail.com>
2024-06-24 08:05:11 -0600

    idk

--- apis/general/urls.py
+++ apis/general/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .api import GetNewId
 
-app_name = "api_app"
+app_name = "api_general"
 urlpatterns = [
     path("get_new_id/", GetNewId.as_view(), name=f"{app_name}_get_new_id"),
 ]
--- front/src/Pages/Index/Loaded/Actions.jsx
+++ front/src/Pages/Index/Loaded/Actions.jsx
@@ -2,31 +2,12 @@ import { useMemo } from 'react';
 import styles from './styles/index.module.scss';
 
 const Actions = props => {
-    const { sobra, hNav } = useMemo(() => {
-        const nav = document.getElementById('nav');
-        const navBar = document.getElementById('navBar');
-        if (!nav || !navBar) return false;
-        const navWidth = nav.offsetWidth;
-        const navBarWidth = navBar.offsetWidth;
-        const sobra = navBarWidth > navWidth;
-        const hNav = nav.offsetHeight;
-    }, [
-        document.getElementById('nav'),
-        document.getElementById('navBar')
-    ]);
 
     return (
         <nav
             id="nav"
             className={`${styles.nav}`}
             >
-            {sobra &&
-            <div 
-                style={{
-                    height: `${hNav}px`
-                }}
-                className={`${styles.navSombraSobra}`}></div>
-            }
             <div id="navBar" className={`${styles.navBar} flex`}>
                 <button className={`${styles.navElement}`}>
                     <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In sapiente possimus</span>
